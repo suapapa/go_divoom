522b27fbcec0036f3d0ea548cabde650ab2a1194
Homin Lee <homin.lee@suapapa.net>
2022-04-07 21:46:26 +0900

    channel control 을 한 파일에 모음

---
+++ channel_control.go
@@ -0,0 +1,163 @@
+package divoom
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+type Channel int
+
+const (
+	ChannelInvalid Channel = iota - 1
+	ChannelFaces
+	ChannelCloud
+	ChannelVisualizer
+	ChannelCustom
+)
+
+func (c *Client) SelectChannel(idx Channel) error {
+	cmd := "Channel/SetIndex"
+	data := map[string]interface{}{
+		"Command":     cmd,
+		"SelectIndex": int(idx),
+	}
+
+	resp, err := c.do(data)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+	defer resp.Body.Close()
+
+	var ret errorCode
+	err = json.NewDecoder(resp.Body).Decode(&ret)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+
+	if ret.ErrorCode != 0 {
+		return fmt.Errorf("fail to select channel: %d", ret.ErrorCode)
+	}
+
+	return nil
+}
+
+type CustomIdx int
+
+const (
+	CustomIdx0 CustomIdx = iota
+	CustomIdx1
+	CustomIdx2
+)
+
+func (c *Client) CustomChannel(idx CustomIdx) error {
+	cmd := "Channel/SetCustomPageIndex"
+	data := map[string]interface{}{
+		"Command":         cmd,
+		"CustomPageIndex": int(idx),
+	}
+
+	resp, err := c.do(data)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+	defer resp.Body.Close()
+
+	var ret errorCode
+	err = json.NewDecoder(resp.Body).Decode(&ret)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+
+	if ret.ErrorCode != 0 {
+		return fmt.Errorf("fail to select channel: %d", ret.ErrorCode)
+	}
+
+	return nil
+}
+
+func (c *Client) VisualizerChannel(idx int) error {
+	cmd := "Channel/SetEqPosition"
+	data := map[string]interface{}{
+		"Command":     cmd,
+		"SelectIndex": idx,
+	}
+
+	resp, err := c.do(data)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+	defer resp.Body.Close()
+
+	var ret errorCode
+	err = json.NewDecoder(resp.Body).Decode(&ret)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+
+	if ret.ErrorCode != 0 {
+		return fmt.Errorf("fail to select channel: %d", ret.ErrorCode)
+	}
+
+	return nil
+}
+
+type CloudChannelIdx int
+
+const (
+	CloudChannelRecommendGallery CloudChannelIdx = iota
+	CloudChannelFacorite
+	CloudChannelSubscribeArtist
+)
+
+func (c *Client) CloudChannel(idx int) error {
+	cmd := "Channel/SetEqPosition"
+	data := map[string]interface{}{
+		"Command":    cmd,
+		"CloudIndex": idx,
+	}
+
+	resp, err := c.do(data)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+	defer resp.Body.Close()
+
+	var ret errorCode
+	err = json.NewDecoder(resp.Body).Decode(&ret)
+	if err != nil {
+		return errors.Wrap(err, "fail to select channel")
+	}
+
+	if ret.ErrorCode != 0 {
+		return fmt.Errorf("fail to select channel: %d", ret.ErrorCode)
+	}
+
+	return nil
+}
+
+type selectIdx struct {
+	SelectIdx int `json:"SelectIndex"`
+}
+
+func (c *Client) GetCurrentChannel() (Channel, error) {
+	cmd := "Channel/GetIndex"
+	data := map[string]interface{}{
+		"Command": cmd,
+	}
+
+	resp, err := c.do(data)
+	if err != nil {
+		return ChannelInvalid, errors.Wrap(err, "fail to get select face id")
+	}
+	defer resp.Body.Close()
+
+	var ret selectIdx
+	err = json.NewDecoder(resp.Body).Decode(&ret)
+	if err != nil {
+		return ChannelInvalid, errors.Wrap(err, "fail to get select face id")
+	}
+
+	return Channel(ret.SelectIdx), nil
+}
