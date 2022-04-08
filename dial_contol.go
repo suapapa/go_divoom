d409764a149ba96a335eec5304c7e5c0939b2d1e
Homin Lee <homin.lee@suapapa.net>
2022-04-08 10:31:21 +0900

    dial_control: add get select face id

--- dial_contol.go
+++ dial_contol.go
@@ -111,3 +111,29 @@ func (c *Client) SelectFacesChannel(id int) error {
 
 	return nil
 }
+
+type FaceID struct {
+	ClockID    int `json:"ClockId"`
+	Brightness int `json:"Brightness"`
+}
+
+func (c *Client) GetSelectFaceID() (*FaceID, error) {
+	cmd := "Channel/SetClockInfo"
+	data := map[string]interface{}{
+		"Command": cmd,
+	}
+
+	resp, err := c.do(data)
+	if err != nil {
+		return nil, errors.Wrap(err, "fail to get select face id")
+	}
+	defer resp.Body.Close()
+
+	var ret FaceID
+	err = json.NewDecoder(resp.Body).Decode(&ret)
+	if err != nil {
+		return nil, errors.Wrap(err, "fail to get select face id")
+	}
+
+	return &ret, nil
+}
